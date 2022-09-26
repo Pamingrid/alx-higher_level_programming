c69060ce7ed9ca585a88c46d0606dad6377ce2f1
Pamingrid <Pamingrid@github.com>
2022-09-26 08:31:53 -0700

    upto 10

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix
-    for col in range(len(line)):
-        print(
-                "{:d}".format(line[col]),
-                end="" if col == len(line) - 1 else " ")
-        print("")
+    for elm in matrix:
+        print(" ".join("{:d}".format(i) for i in elm))
