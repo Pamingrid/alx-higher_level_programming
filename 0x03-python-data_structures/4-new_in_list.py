0af578978651f57674c46ff9fccd9db80359f698
Pamingrid <Pamingrid@github.com>
2022-09-26 08:05:32 -0700

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (0 <= idx < len(my_list)):
-        my_list[idx] != element
-    return (new_in_list)
+    if not my_list:
+        return
+    new_list = my_list.copy()
+    if 0 <= idx < len(my_list)):
+        new_list[idx] = element
+    return (new_list)
